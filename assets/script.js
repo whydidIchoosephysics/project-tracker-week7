a0d12d0625e0db103145e448e2afce0aa78f16cb
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-02-03 11:58:18 +0000

    Made timeslot colouring dynamic based on the time, can save input for fist timeslot into local storage

--- assets/script.js
+++ assets/script.js
@@ -8,57 +8,89 @@ function getCurrentDate() {
   let dateNow = moment().format("dddd, MMMM Do: H:mm:ss");
 
   currentDate.text(dateNow);
-  // setInterval(getCurrentDate, 1000);
+  setInterval(getCurrentDate, 1000);
 }
 
 getCurrentDate();
 
-let allTimes = [];
+// let allTimes = [];
 
-function createTimeSlots() {
-  // Add hour to the cards
-  let t = {
-    slotInterval: 60,
-    startTime: "9:00",
-    finishTime: "17:00",
-  };
+// function createTimeSlots() {
+//   // Add hour to the cards
+//   let t = {
+//     slotInterval: 60,
+//     startTime: "9:00",
+//     finishTime: "17:00",
+//   };
 
-  //Format the time
-  let startTime = moment(t.startTime, "HH:mm");
-  let endTime = moment(t.finishTime, "HH:mm");
+//   //Format the time
+//   let startTime = moment(t.startTime, "HH:mm");
+//   let endTime = moment(t.finishTime, "HH:mm");
 
-  //Loop over the times - only pushes time with 60 minutes interval
-  while (startTime < endTime) {
-    //Push times
-    allTimes.push(startTime.format("HH:mm"));
+//   //Loop over the times - only pushes time with 60 minutes interval
+//   while (startTime < endTime) {
+//     //Push times
+//     allTimes.push(startTime.format("HH:mm"));
 
-    //Add interval of 60 minutes
-    startTime.add(t.slotInterval, "minutes");
-  }
+//     //Add interval of 60 minutes
+//     startTime.add(t.slotInterval, "minutes");
+//   }
 
-  return allTimes;
-}
+//   return allTimes;
+// }
 
-createTimeSlots();
-console.log(allTimes);
+// createTimeSlots();
+// console.log(allTimes);
 
 // $(hourlySlots).each(function () {
 //   let lastElement = allTimes.slice(-1);
 //   $(this).text(lastElement);
 // });
 
-// let Times = [
-//   "09:00",
-//   "10:00",
-//   "11:00",
-//   "12:00",
-//   "13:00",
-//   "14:00",
-//   "15:00",
-//   "16:00",
-// ];
+let Times = [
+  "09:00",
+  "10:00",
+  "11:00",
+  "12:00",
+  "13:00",
+  "14:00",
+  "15:00",
+  "16:00",
+];
 
-for (hour in allTimes) {
-  let lastElement = allTimes[hour];
+for (hour in Times) {
+  let lastElement = Times[hour];
   $(hourlySlots[hour]).text(lastElement);
 }
+
+let currentHour = moment().hour();
+console.log(currentHour);
+
+$(hourlySlots).each(function () {
+  let currentTimeSlot = parseInt($(this).text().split(":")[0]);
+  console.log(currentTimeSlot);
+
+  let toDoText = localStorage.getItem(currentTimeSlot);
+  $(this).siblings(".description").val(toDoText);
+
+  if (currentTimeSlot < currentHour) {
+    $(this).parent().parent().addClass("past");
+  } else if (currentTimeSlot === currentHour) {
+    $(this).parent().parent().removeClass("past");
+    $(this).parent().parent().addClass("present");
+  } else {
+    $(this).parent().parent().removeClass("past present");
+    $(this).parent().parent().addClass("future");
+  }
+});
+
+$(".card-link").on("click", function () {
+  let value = $(this).parent().siblings(".description").val().trim();
+  console.log(value);
+
+  let currentTimeSlot = parseInt(
+    $(this).parent().siblings(".card-title").text().split(":")[0]
+  );
+  console.log(currentTimeSlot);
+  localStorage.setItem(currentTimeSlot, value);
+});
